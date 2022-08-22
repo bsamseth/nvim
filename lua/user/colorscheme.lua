e3d822c88acee166ce205bbf884da3311c0d79af
Bendik Samseth <b.samseth@gmail.com>
2022-08-22 06:51:43 +0200

    use tokyionight as colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,4 +1,5 @@
-local colorscheme = "darkplus"
+vim.g.tokyonight_style = "night"
+local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
