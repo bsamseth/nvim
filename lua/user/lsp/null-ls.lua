8b57cc3c386084d27162b734e6b0f65d8d7d46a5
Bendik Samseth <b.samseth@gmail.com>
2022-08-23 07:52:55 +0200

    add isort to null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -9,11 +9,12 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.stylua,
-    diagnostics.flake8,
-  },
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.isort.with({ extra_args = { "--profile", "black" } }),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		formatting.stylua,
+		diagnostics.flake8,
+	},
+})
