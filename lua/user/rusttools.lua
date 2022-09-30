05fdbfad16184d2268151c64bcbf4586193b3fe5
Bendik Samseth <b.samseth@gmail.com>
2022-09-30 13:47:07 +0200

    rust: try to get more rust tooling to work

--- lua/user/rusttools.lua
+++ lua/user/rusttools.lua
@@ -7,10 +7,16 @@ end
 local on_attach = require("user.lsp.handlers").on_attach
 local capabilities = require("user.lsp.handlers").capabilities
 
+local codelldb = "/home/bendik/.local/share/nvim/dapinstall/codelldb/extension/adapter/codelldb"
+local liblldb = "/home/bendik/.local/share/nvim/dapinstall/codelldb/extension/lldb/lib/liblldb.so"
+
 rt.setup({
   server = {
     on_attach = on_attach,
     capabilities = capabilities,
+  },
+  dap = {
+    adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb, liblldb),
   },
 })
 
